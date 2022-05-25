742488e143671f6aa40ee9a18bd55daca4950422
amoralesc <a.moralesc@javeriana.edu.co>
2022-05-24 19:31:18 -0500

    Pass business logic of the Sensor to the entity

--- Sensor/src/main/java/org/example/ControladorSensor.java
+++ Sensor/src/main/java/org/example/ControladorSensor.java
@@ -1,14 +1,6 @@
 package org.example;
 
-import org.zeromq.SocketType;
-import org.zeromq.ZContext;
-import org.zeromq.ZMQ;
-import org.zeromq.ZMQException;
-
 public class ControladorSensor {
-	@SuppressWarnings("FieldCanBeLocal")
-	private static Sensor sensor;
-
 	 /**
 	 * args[0]: tipo de sensor en mayúsculas,
 	 * args[1]: ruta del archivo de configuración del sensor,
@@ -19,43 +11,7 @@ public class ControladorSensor {
 		System.out.println("Ruta archivo configuración: " + args[1]);
 		System.out.println("Intervalo t de medidas: " + args[2]);
 
-		sensor = new Sensor(args[0], args[1], Long.parseLong(args[2]));
-
-		try (ZContext context = new ZContext()) {
-			// CONEXIÓN CON LOS MONITORES (PUB - SUB)
-			// El sensor actúa como publisher
-			ZMQ.Socket publisher = context.createSocket(SocketType.PUB);
-
-			// Buscar un puerto disponible y hacer un bind a este
-			boolean encuentraPuerto = false;
-			for (int i = sensor.getTipoSensor().primerPuerto; i < sensor.getTipoSensor().ultimoPuerto; i++) {
-				try {
-					publisher.bind(String.format("tcp://*:%d", i));
-					System.out.printf("\nEl sensor ha hecho bind al puerto: %d%n", i);
-					encuentraPuerto = true;
-					break;
-				} catch (ZMQException addressAlreadyInUseException) {
-					// Puerto está siendo utilizado
-				}
-			}
-			// Si no encuentra ningún puerto, el sensor no puede funcionar
-			if (!encuentraPuerto) {
-				System.out.println("\nTodos los puertos están ocupados y el sensor no puede ejecutarse");
-				System.exit(1);
-			}
-
-			System.out.println("El sensor se está ejecutando y enviando medidas...");
-			System.out.println("Ingrese CTRL + C para terminar el proceso...");
-
-			// El sensor empieza a enviar medidas mientras no se interrumpa el proceso
-			while (!Thread.currentThread().isInterrupted()) {
-				publisher.sendMore(sensor.getTipoSensor().tema);    // Tema (tipo de medida)
-				publisher.send(sensor.generarMedida().toString());  // Mensaje (medida)
-
-				//noinspection BusyWait
-				Thread.sleep(sensor.getIntervalo());    // El sensor envía medidas cada intervalo t
-			}
-			publisher.close();
-		}
+		Sensor sensor = new Sensor(args[0], args[1], Long.parseLong(args[2]));
+		sensor.ejecutar();
 	}
 }
--- Sensor/src/main/java/org/example/Sensor.java
+++ Sensor/src/main/java/org/example/Sensor.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQException;
+
 import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -113,5 +118,49 @@ public class Sensor {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd/HH:mm:ss.SSS");
 		LocalDateTime now = LocalDateTime.now();
 		return new Medida(this.tipoSensor.sensor, dtf.format(now), valor, this.tipoSensor.unidadMedida);
+	}
+
+	/**
+	 * Empieza a ejecutar el sensor por siempre hasta ser interrumpido
+	 *
+	 * @throws InterruptedException si el hilo es interrumpido
+	 */
+	public void ejecutar() throws InterruptedException {
+		try (ZContext context = new ZContext()) {
+			// CONEXIÓN CON LOS MONITORES (PUB - SUB)
+			// El sensor actúa como publisher
+			ZMQ.Socket publisher = context.createSocket(SocketType.PUB);
+
+			// Buscar un puerto disponible y hacer un bind a este
+			boolean encuentraPuerto = false;
+			for (int i = this.getTipoSensor().primerPuerto; i < this.getTipoSensor().ultimoPuerto; i++) {
+				try {
+					publisher.bind(String.format("tcp://*:%d", i));
+					System.out.printf("\nEl sensor ha hecho bind al puerto: %d%n", i);
+					encuentraPuerto = true;
+					break;
+				} catch (ZMQException addressAlreadyInUseException) {
+					// Puerto está siendo utilizado
+				}
+			}
+			// Si no encuentra ningún puerto, el sensor no puede funcionar
+			if (!encuentraPuerto) {
+				System.out.println("\nTodos los puertos están ocupados y el sensor no puede ejecutarse");
+				System.exit(1);
+			}
+
+			System.out.println("El sensor se está ejecutando y enviando medidas...");
+			System.out.println("Ingrese CTRL + C para terminar el proceso...");
+
+			// El sensor empieza a enviar medidas mientras no se interrumpa el proceso
+			while (!Thread.currentThread().isInterrupted()) {
+				publisher.sendMore(this.getTipoSensor().tema);    // Tema (tipo de medida)
+				publisher.send(this.generarMedida().toString());  // Mensaje (medida)
+
+				//noinspection BusyWait
+				Thread.sleep(this.getIntervalo());    // El sensor envía medidas cada intervalo t
+			}
+			publisher.close();
+		}
 	}
 }
